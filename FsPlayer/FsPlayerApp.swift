337a800683c41358ca30712c8bb81d9e489c9b65
Fabiano <jeovajire2003@gmail.com>
2026-05-26 17:29:12 -0300

    Initial Commit

---
+++ FsPlayer/FsPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FsPlayerApp.swift
+//  FsPlayer
+//
+//  Created by Fabiano on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct FsPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
